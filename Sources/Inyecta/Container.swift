d491a55c9a9f52dfcf252cd7608a67ade914b593
Juan David Hurtado <juandahurtadom@gmail.com>
2024-09-09 11:55:06 -0500

    Add service key

--- Sources/Inyecta/Container.swift
+++ Sources/Inyecta/Container.swift
@@ -5,6 +5,8 @@
 //  Created by Juan David Hurtado on 8/09/24.
 //
 
+import Foundation
+
 // TODO: check if we might need to return something here
 // TODO: add register options: singleton and factory
 // Describes the register capabitlity
@@ -23,23 +25,38 @@ public protocol ContainerResolving {
     func resolve<T>(_ type: T.Type) -> T?
 }
 
+struct ServiceKey {
+    let id = UUID().uuidString
+    let type: Any.Type
+    // TODO: add options
+}
+
+extension ServiceKey: Hashable {
+    static func == (lhs: ServiceKey, rhs: ServiceKey) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
 /// Describes a container.
 public class Container {
-    // TODO: find a proper way of a key for the dictionary
     // TODO: maybe we need to store the factory?
-    var services: [UInt: Any] = [:]
+    var services: [ServiceKey: Any] = [:]
 }
 
 // MARK: - Register
 extension Container: ContainerRegistering {
     public func register<T>(_ factory: @escaping () -> T) {
-        services[0] = factory()
+        services[.init(type: T.self)] = factory()
     }
 }
 
 extension Container: ContainerResolving {
     public func resolve<T>(_ type: T.Type) -> T? {
-        guard let service = services.first(where: { $0.value is T })?.value else {
+        guard let service = services.first(where: { $0.key.type == type })?.value else {
             // TODO: check how to notify user of error
             return nil
         }
