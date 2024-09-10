0772c14495e19b06f6805c8204dc6d2c4a5de387
Juan David Hurtado <juandahurtadom@gmail.com>
2024-09-10 11:07:25 -0500

    Add registration type

--- Tests/InyectaTests/InyectaTests.swift
+++ Tests/InyectaTests/InyectaTests.swift
@@ -2,17 +2,61 @@ import XCTest
 @testable import Inyecta
 
 final class InyectaTests: XCTestCase {
+    var container: Container!
+    
+    override func setUp() {
+        container = Container()
+    }
+    
     func testContainer_afterRegisterAService_shouldReturnService() throws {
-        let container = Container()
         container.register {
             Mock()
         }
         let mock = container.resolve(Mock.self)
         XCTAssertNotNil(mock)
     }
+    
+    func testResolve_whenRegisteringAsFactory_shouldReturnDifferentInstances() {
+        container.register(.factory) {
+            MockClass()
+        }
+        
+        guard let firstInstance = container.resolve(MockClass.self) else {
+            XCTFail("instance was not found")
+            return
+        }
+        guard let secondInstance = container.resolve(MockClass.self) else {
+            XCTFail("instance was not found")
+            return
+        }
+        
+        XCTAssertNotEqual(firstInstance.id, secondInstance.id)
+    }
+    
+    func testResolve_whenRegisteringAsSingleton_shouldReturnTheSameInstace() {
+        container.register(.singleton) {
+            MockClass()
+        }
+        
+        guard let firstInstance = container.resolve(MockClass.self) else {
+            XCTFail("instance was not found")
+            return
+        }
+        guard let secondInstance = container.resolve(MockClass.self) else {
+            XCTFail("instance was not found")
+            return
+        }
+        
+        XCTAssertEqual(firstInstance.id, secondInstance.id)
+    }
 }
 
 // MARK: - Helper
 struct Mock {
     var mock = "hi"
+}
+
+
+class MockClass {
+    let id = UUID().uuidString
 }
